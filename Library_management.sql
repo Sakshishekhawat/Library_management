c43dbdecc2e09cfd9794caa523e1ae442a52b0e9
Sakshishekhawat <143182859+Sakshishekhawat@users.noreply.github.com>
2023-09-11 10:10:41 +0530

    Update Library_management.sql

--- Library_management.sql
+++ Library_management.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE  library_management;
 
-CREATE TABLE IF NOT EXISTS library_management.user_login (
+CREATE TABLE  library_management.user_login (
 	user_id TEXT PRIMARY KEY,
     user_password TEXT,
     first_name TEXT,
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS library_management.user_login (
 	email_id TEXT
 );
 
-CREATE TABLE IF NOT EXISTS library_management.publisher (
+CREATE TABLE  library_management.publisher (
 	publisher_id TEXT PRIMARY KEY,
     publisher TEXT,
     distributor TEXT,
@@ -17,14 +17,14 @@ CREATE TABLE IF NOT EXISTS library_management.publisher (
 	last_release DATE
 );
 
-CREATE TABLE IF NOT EXISTS library_management.author (
+CREATE TABLE  library_management.author (
 	author_id TEXT PRIMARY KEY,
 	first_name TEXT,
 	last_name TEXT,
 	publications_count INT
 );
 
-CREATE TABLE IF NOT EXISTS library_management.books (
+CREATE TABLE  library_management.books (
 	book_id TEXT PRIMARY KEY,
     book_code TEXT,
     book_name TEXT,
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS library_management.books (
     is_available BOOLEAN
 );
 
-CREATE TABLE IF NOT EXISTS library_management.staff (
+CREATE TABLE  library_management.staff (
 	staff_id TEXT PRIMARY KEY,
     first_name TEXT,
 	last_name TEXT,
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS library_management.staff (
 	work_shift_end TIME
 );
 
-CREATE TABLE IF NOT EXISTS library_management.readers (
+CREATE TABLE  library_management.readers (
 	reader_id TEXT PRIMARY KEY,
     first_name TEXT,
 	last_name TEXT,
@@ -61,7 +61,7 @@ CREATE TABLE IF NOT EXISTS library_management.readers (
 	current_fine FLOAT
 );
 
-CREATE TABLE IF NOT EXISTS library_management.books_issue (
+CREATE TABLE  library_management.books_issue (
 	issue_id SERIAL PRIMARY KEY,
 	book_id TEXT REFERENCES library_management.books (book_id),
 	issued_to TEXT REFERENCES library_management.readers (reader_id),
@@ -72,7 +72,7 @@ CREATE TABLE IF NOT EXISTS library_management.books_issue (
 	payment_transaction_id TEXT
 );
 
-CREATE TABLE IF NOT EXISTS library_management.settings (
+CREATE TABLE  library_management.settings (
 	book_issue_count_per_reader INT,
 	fine_per_day FLOAT,
 	book_return_in_days INT
